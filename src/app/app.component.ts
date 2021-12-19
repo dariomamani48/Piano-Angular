1f8a8d75bcb011f38de7ad94c5799e06396940ca
dariomamani48 <dariomamani48@gmail.com>
2021-12-18 22:21:07 -0300

    Priemr comit con el piano funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'piano-angular';
+  title = 'piano-angular';// titulo que se muestra en el navegador
+
+
+  suenaNota(numero: number): void{ //se crea un metedo para hacer sonas las noas guardadas
+      var audio = new Audio;// creamis una nueva instancia de audio
+      audio.src ="../assets/sonidos/note"+ numero+ ".wav"// buscamos la fuente del audio
+      audio.load();//carga el audio con el metodo load del objeto audio
+      audio.play();//permite que el audio se repoduzca
+  }
 }
